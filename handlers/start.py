2a419e5bd8c86093e3ba9edd88666b332a4704d0
subsmaart-cyber <subsmaart@gmail.com>
2026-07-30 19:19:30 +0600

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -3,12 +3,21 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 
 from keyboards.main_menu import main_menu
+from database import add_user
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def start(message: Message):
+    user = message.from_user
+
+    await add_user(
+        user.id,
+        user.username,
+        user.full_name
+    )
+
     await message.answer(
         "👋 Welcome to Subs Mart!\n\n"
         "Use the menu below to explore the shop.",
